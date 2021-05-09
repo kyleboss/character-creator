6f6e65f683a96b64f01896c2b0d1a36ab0448bd6
Kyle Boss <kyle@kyleboss.com>
2021-05-09 12:08:42 -0700

    Finished!

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,9 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveEffect}></div>
+
+      <div className={styles.perspective} />
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -77,7 +80,6 @@ function App() {
           />
         </div>
       </MaxWidthWrapper>
-
       <div className={styles.characterWrapper}>
         <Character
           body={body}
